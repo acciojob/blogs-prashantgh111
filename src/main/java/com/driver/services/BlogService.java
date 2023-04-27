118f11fc813a8af5ca9c47c4efb30fa7e39b3fa8
prashantgh111 <107759833+prashantgh111@users.noreply.github.com>
2023-04-28 01:26:39 +0530

    Blog assign added

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -1,17 +1,13 @@
 package com.driver.services;
 
 import com.driver.models.Blog;
-import com.driver.models.Image;
 import com.driver.models.User;
 import com.driver.repositories.BlogRepository;
-import com.driver.repositories.ImageRepository;
 import com.driver.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Service
 public class BlogService {
@@ -23,11 +19,19 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
-
+        User user=userRepository1.findById(userId).get();
+        Blog blog = new Blog();
+        blog.setTitle(title);
+        blog.setContent(content);
+        blog.setPubDate(new Date());
+        blog.setUser(user);
+        user.getBlogList().add(blog);
+        userRepository1.save(user);
+        return blog;
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
-
+        blogRepository1.deleteById(blogId);
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,33 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
-
+        Blog blog = blogRepository2.findById(blogId).get();
+        Image image = new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+        image.setBlog(blog);
+        blog.getImageList().add(image);
+        blogRepository2.save(blog);
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
-        //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
 
+        //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        String[] screen_arr = screenDimensions.split("X");
+        int screen_length = Integer.parseInt(screen_arr[0]);
+        int screen_breadth = Integer.parseInt(screen_arr[1]);
+
+        Image image = imageRepository2.findById(id).get();
+
+        String[] image_arr = image.getDimensions().split("X");
+        int image_length = Integer.parseInt(image_arr[0]);
+        int image_breadth = Integer.parseInt(image_arr[1]);
+
+        return (screen_length/image_length)*(screen_breadth/image_breadth);
     }
 }
